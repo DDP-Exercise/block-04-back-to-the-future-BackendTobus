1e2bd76002c5b29c753f36a8c33b2b1c2f9be079
Tobi <tobias2004.h@gmail.com>
2026-04-23 13:00:49 +0200

    block4_Hofinger_Tobias

--- js/view.digital.js
+++ js/view.digital.js
@@ -1,1 +1,20 @@
 "use strict";
+
+export let digitalView = {
+    currentDoc : HTMLLabelElement,
+    init: function (time) {
+        this.currentDoc = document.createElement("label");
+        this.currentDoc.textContent = time;
+        document.body.append(this.currentDoc);
+        this.addButton();
+    },
+    update: function (time) {
+        this.currentDoc.textContent = time;
+    },
+    addButton: function() {
+        let btn = document.createElement("button")
+        btn.textContent = "Save Timestamp";
+        document.body.append(btn);
+        btn.addEventListener("click",{})
+    },
+}
